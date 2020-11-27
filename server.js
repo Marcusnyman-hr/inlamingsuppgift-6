92f5be6297411164f735c8ecd7970bb3e99ad73e
Marcus Nyman <Marcusnyman556@gmail.com>
2020-11-27 13:16:10 +0100

    trying helmet12

--- server.js
+++ server.js
@@ -26,31 +26,30 @@ mongoose.connect(process.env.DB_CONNECT,
 app.use(express.json())
 app.use(cors());
 app.use(
+  // [
   helmet.contentSecurityPolicy({
     directives: {
       defaultSrc: ["'self'"],
-      connectSrc: ["'self'"],
-      frameSrc: ["'self'"],
-      childSrc: ["'self'"],
-      scriptSrc: ["'self'", 'unsafe-inline', 'https://kit.fontawesome.com'],
-      scriptSrcElem: ["'self'", 'unsafe-inline', 'https://kit.fontawesome.com'],
+      connectSrc: ["'self'", 'https://checkout.stripe.com'],
+      frameSrc: ["'self'", 'https://checkout.stripe.com'],
+      childSrc: ["'self'", 'https://checkout.stripe.com'],
+      scriptSrc: ["'self'", 'https://checkout.stripe.com'],
       styleSrc: [
         "'self'",
-        'unsafe-inline',
         'https://fonts.googleapis.com',
-
+        'https://checkout.stripe.com',
       ],
-      fontSrc: ["'self'", 'https://fonts.gstatic.com/'],
-      imgSrc: ["'self'"],
+      fontSrc: ["'self'", 'https://fonts.gstatic.com'],
+      imgSrc: ["'self'", 'https://*.stripe.com', 'https://res.cloudinary.com'],
       baseUri: ["'self'"],
-      connectSrc: ["'self', 'https://ka-f.fontawesome.com/'"],
     },
   })
-);
-app.use((req, res, next) => {
-  console.log('CSP', res.get('Content-Security-Policy'));
-  next();
-});
+  // ]
+)
+// app.use((req, res, next) => {
+//   console.log('CSP', res.get('Content-Security-Policy'));
+//   next();
+// });
 
 //Serve static assets if in prod
 if (process.env.NODE_ENV === 'production') {
