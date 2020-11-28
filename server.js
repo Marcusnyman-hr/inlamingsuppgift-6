be5bcaeca12fe41927a7d151c9ca11d23868e05b
Marcus Nyman <Marcusnyman556@gmail.com>
2020-11-28 09:57:33 +0100

    trying helme33343ssss9

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(
       defaultSrc: ["'self'"],
       scriptSrc: ["'self'", "https://kit.fontawesome.com"],
       objectSrc: ["'none'"],
-      scriptSrcElem: ["'self'", "'unsafe-inline'", "https://kit.fontawesome.com"],
+      scriptSrcElem: ["'self'", "https://kit.fontawesome.com"],
       styleSrcElem: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com", "https://kit.fontawesome.com"],
       connectSrc: ["'self'", "https://ka-f.fontawesome.com"],
       fontSrc: ["'self'", "https://fonts.gstatic.com", "https://ka-f.fontawesome.com"],
