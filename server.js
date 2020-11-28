f1edfcd3953c466ca9169aa85a6ce6d6dd4e5e92
Marcus Nyman <Marcusnyman556@gmail.com>
2020-11-28 09:52:50 +0100

    trying helme33343ssss9

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(
   helmet.contentSecurityPolicy({
     directives: {
       defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", "'unsafe-inline'", "https://kit.fontawesome.com"],
+      scriptSrc: ["'self'", "https://kit.fontawesome.com"],
       objectSrc: ["'none'"],
       scriptSrcElem: ["'self'", "'unsafe-inline'", "https://kit.fontawesome.com"],
       styleSrcElem: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com", "https://kit.fontawesome.com"],
@@ -39,6 +39,23 @@ app.use(
     },
   })
 );
+
+// app.use(express.json())
+// app.use(cors());
+// app.use(
+//   helmet.contentSecurityPolicy({
+//     directives: {
+//       defaultSrc: ["'self'"],
+//       scriptSrc: ["'self'", "'unsafe-inline'", "https://kit.fontawesome.com"],
+//       objectSrc: ["'none'"],
+//       scriptSrcElem: ["'self'", "'unsafe-inline'", "https://kit.fontawesome.com"],
+//       styleSrcElem: ["'self'", "'unsafe-inline'", "https://fonts.googleapis.com", "https://kit.fontawesome.com"],
+//       connectSrc: ["'self'", "https://ka-f.fontawesome.com"],
+//       fontSrc: ["'self'", "https://fonts.gstatic.com", "https://ka-f.fontawesome.com"],
+//       upgradeInsecureRequests: [],
+//     },
+//   })
+// );
 
 
 
